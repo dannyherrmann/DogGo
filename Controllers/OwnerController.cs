fb650eb7a45beecb8b7587bc840b590c8fa5151c
Danny <dannyherrmann91@gmail.com>
2023-04-06 21:22:57 -0500

    updates

--- Controllers/OwnerController.cs
+++ Controllers/OwnerController.cs
@@ -19,6 +19,7 @@ namespace DogGo.Controllers
             return View(owners);
         }
 
+        // details does not work if owner has no dogs yet
         public ActionResult Details(int id)
         {
             Owner owner = _ownerRepo.GetOwnerById(id);
@@ -29,6 +30,79 @@ namespace DogGo.Controllers
             }
 
             return View(owner);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Owner owner)
+        {
+            try
+            {
+                _ownerRepo.AddOwner(owner);
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(owner);
+            }
+        }
+
+        // Delete does not work if owner does not have any dogs yet
+        public ActionResult Delete(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            return View(owner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Owner owner)
+        {
+            try
+            {
+                _ownerRepo.DeleteOwner(owner.Id);
+
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                return View(owner);
+            }
+        }
+
+        // Edit does not work if owner does not have any dogs yet
+        public ActionResult Edit(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            if (owner == null)
+            {
+                return NotFound();
+            }
+
+            return View(owner);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Owner owner)
+        {
+            try 
+            {
+                _ownerRepo.UpdateOwner(owner);
+
+                return RedirectToAction("Index");
+            }
+            catch(Exception ex)
+            {
+                return View(owner);
+            }
         }
     }
 }
--- Repositories/OwnerRepository.cs
+++ Repositories/OwnerRepository.cs
@@ -97,6 +97,7 @@ namespace DogGo.Repositories
                                 Name = DbUtils.GetString(reader, "OwnerName"),
                                 Address = DbUtils.GetString(reader, "OwnerAddress"),
                                 Phone = DbUtils.GetString(reader, "OwnerPhone"),
+                                NeighborhoodId = DbUtils.GetInt(reader, "NeighborhoodId"),
                                 Dogs = new List<Dog>(),
                                 Neighborhood = new Neighborhood
                                 {
@@ -123,6 +124,130 @@ namespace DogGo.Repositories
 
                     reader.Close();
                     return owner;
+                }
+            }
+        }
+
+        public Owner GetOwnerByEmail(string email)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    select  o.Id as OwnerId,
+                            o.Email as OwnerEmail,
+                            o.[Name] as OwnerName,
+                            o.Address as OwnerAddress,
+                            o.Phone as OwnerPhone,
+                            n.Id as NeighborhoodId,
+                            n.Name as NeighborhoodName,
+                        FROM Owner o
+                        JOIN Neighborhood n on o.NeighborhoodId = n.Id
+                        WHERE o.Email = @email";
+
+                    DbUtils.AddParameter(cmd, "@email", email);
+
+                    var reader = cmd.ExecuteReader();
+
+                    Owner owner = null;
+
+                    if (reader.Read())
+                    {
+                        owner = new Owner()
+                        {
+                            Id = DbUtils.GetInt(reader, "OwnerId"),
+                            Email = DbUtils.GetString(reader, "OwnerEmail"),
+                            Name = DbUtils.GetString(reader, "OwnerName"),
+                            Address = DbUtils.GetString(reader, "OwnerAddress"),
+                            Phone = DbUtils.GetString(reader, "OwnerPhone"),
+                            Dogs = new List<Dog>(),
+                            Neighborhood = new Neighborhood
+                            {
+                                Id = DbUtils.GetInt(reader, "NeighborhoodId"),
+                                Name = DbUtils.GetString(reader, "NeighborhoodName")
+                            }
+                        };
+                    }
+
+                    reader.Close();
+                    return owner;
+                }
+            }
+        }
+
+        public void AddOwner(Owner owner)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                    INSERT INTO Owner ([Name], Email, Phone, Address, NeighborhoodId)
+                    OUTPUT INSERTED.ID
+                    VALUES (@name, @email, @phoneNumber, @address, @neighborhoodId);";
+
+                    cmd.Parameters.AddWithValue("@name", owner.Name);
+                    cmd.Parameters.AddWithValue("@email", owner.Email);
+                    cmd.Parameters.AddWithValue("@phoneNumber", owner.Phone);
+                    cmd.Parameters.AddWithValue("@address", owner.Address);
+                    cmd.Parameters.AddWithValue("@neighborhoodId", owner.NeighborhoodId);
+
+                    int id = (int)cmd.ExecuteScalar();
+
+                    owner.Id = id;
+                }
+            }
+        }
+
+        public void UpdateOwner(Owner owner)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            UPDATE Owner
+                            SET 
+                                [Name] = @name, 
+                                Email = @email, 
+                                Address = @address, 
+                                Phone = @phone, 
+                                NeighborhoodId = @neighborhoodId
+                            WHERE Id = @id";
+
+                    cmd.Parameters.AddWithValue("@name", owner.Name);
+                    cmd.Parameters.AddWithValue("@email", owner.Email);
+                    cmd.Parameters.AddWithValue("@address", owner.Address);
+                    cmd.Parameters.AddWithValue("@phone", owner.Phone);
+                    cmd.Parameters.AddWithValue("@neighborhoodId", owner.NeighborhoodId);
+                    cmd.Parameters.AddWithValue("@id", owner.Id);
+
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public void DeleteOwner(int ownerId)
+        {
+            using (var conn = Connection)
+            {
+                conn.Open();
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"
+                            DELETE FROM Owner
+                            WHERE Id = @id
+                        ";
+
+                    cmd.Parameters.AddWithValue("@id", ownerId);
+
+                    cmd.ExecuteNonQuery();
                 }
             }
         }
